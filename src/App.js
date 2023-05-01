5ef48867efff4f06926373a2b7b3185b4d9b0837
Yodgorbek <yodgorbeksheraliyev777@gmail.com>
2023-05-01 07:45:36 +0500

    to do list create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header/Header';
+import ToDoList from './Components/ToDoList/ToDoList';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <ToDoList/>
     </div>
   );
 }
